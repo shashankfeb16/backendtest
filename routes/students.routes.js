34e5ea1122744c5dcedc70f21059c20fb1ba3fad
shashankfeb16 <shashankfeb16@gmail.com>
2022-10-03 19:47:21 +0530

    changes

--- routes/students.routes.js
+++ routes/students.routes.js
@@ -21,7 +21,7 @@ studentController.post("/student/create",async(req,res)=>{
 studentController.get("/getallstudents",async(req,res)=>{
     const student = await StudentModel.find();
     if(student){
-        res.send(student)
+       return res.send(student)
     }
 });
 
